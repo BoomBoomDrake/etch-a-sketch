355dc3413b096a37215a43ab490a2242a3235b65
eric <eric.drake2011@gmail.com>
2022-08-16 15:15:36 -0500

    Add default grid generator and slider input

--- README.md
+++ README.md
@@ -2,13 +2,15 @@ Etch-A-Sketch App
 
 For The Odin Project: https://www.theodinproject.com/lessons/foundations-etch-a-sketch
 
+Finished Product Should:
+
+
 Things To Do:
 
     HTML:
         * Insert slider input for grid dimensions
 
     CSS:
-        * Align structural components into final arrangement
         * Choose color theme
         * Round off button borders
         * Grid:
--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const container = document.createElement('div');
+// UI Targets
+//
+const colorPicker = document.getElementById('colorPicker');
+const blackBtn = document.getElementById('blackBtn');
+const rainbowBtn = document.getElementById('rainbowBtn');
+const shadeBtn = document.getElementById('shadeBtn');
+const eraserBtn = document.getElementById('eraserBtn');
+const grid = document.getElementById('grid');
+const clearbtn = document.getElementById('clearbtn');
+const gridSlider = document.getElementById('gridSlider');
 
-const title = document.createElement('h1');
+// Default Grid Generation
+function genGrid(rows) {
+    grid.style.setProperty('--grid-rows', rows);
+    grid.style.setProperty('--grid-cols', rows);
+    for (let c = 0; c < (rows * rows); c++) {
+        let cell = document.createElement('div');
+        cell.className = 'cell';
+        grid.appendChild(cell);
+    }
+}
+genGrid(16);
+
+
