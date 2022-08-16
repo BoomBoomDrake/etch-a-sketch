32732028e7d0ff2f09474ad99781e3110e43459d
eric <eric.drake2011@gmail.com>
2022-08-16 16:30:18 -0500

    Add clear grid function

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const rainbowBtn = document.getElementById('rainbowBtn');
 const shadeBtn = document.getElementById('shadeBtn');
 const eraserBtn = document.getElementById('eraserBtn');
 const grid = document.getElementById('grid');
+let cell;
 const clearbtn = document.getElementById('clearbtn');
 
 const gridSlider = document.getElementById('gridSlider');
@@ -17,11 +18,25 @@ function genGrid(rows) {
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', rows);
     for (let c = 0; c < (rows * rows); c++) {
-        let cell = document.createElement('div');
+        cell = document.createElement('div');
         cell.className = 'cell';
         grid.appendChild(cell);
     }
 }
 genGrid(16);
 
+function clearGrid(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
+//Grid slider function
+gridSlider.addEventListener('input', () => {
+    gridSliderLabel.textContent = `${gridSlider.value} X ${gridSlider.value}`;
+
+    gridSlider.addEventListener('mouseup', () => {
+        clearGrid(grid);
+        genGrid(gridSlider.value);
+    })
+})
