8eb867365d25cbe24ca1e04bb44ebb574163153d
eric <eric.drake2011@gmail.com>
2022-08-18 10:32:11 -0500

    Add random color function and colorbtn function

--- script.js
+++ script.js
@@ -30,13 +30,54 @@ genGrid(16);
 
 function listen() {
     for (let i = 0; i < cellArray.length; i++) {
-        cellArray[i].addEventListener('click', () =>{
-            cellArray[i].style.backgroundColor = 'black';
-        });
+        cellArray[i].addEventListener('mouseenter', draw);
     }
 }
 
+// Default ink color
+let ink = 'black'
+function draw(e) {
+    if (black || color) {
+        e.target.style.backgroundColor = ink;
+        e.target.style.borderTop = ink;
+        e.target.style.borderRight = ink;
+    } else if (random) {
+       randomColor();
+       e.target.style.backgroundColor = ink;
+       e.target.style.borderTop = ink;
+       e.target.style.borderRight = ink;
+    } else if (eraser) {
+        e.target.style.backgroundColor = ink;
+        e.target.style.borderTop = '#ddd';
+        e.target.style.borderRight = '#ddd';
+    }
+}
 
+function randomColor() {
+    num = Math.floor(Math.random() * 16777215).toString(16);
+    ink = "#" + num;
+    //console.log(ink);
+}
+
+let color = false
+colorPicker.addEventListener('change', () => {
+    // toggle color button selection
+    if (!color) {
+        color = true;
+        black = false;
+        random = false;
+        shade = false;
+        erase = false;
+    // toggle button highlight
+        colorPicker.classList.add('btn-on');
+        blackBtn.classList.remove('btn-on');
+        rainbowBtn.classList.remove('btn-on');
+        shadeBtn.classList.remove('btn-on');
+        eraserBtn.classList.remove('btn-on');
+    }
+
+    ink = colorPicker.value
+})
 //Grid slider function
 gridSlider.addEventListener('input', () => {
     gridSliderLabel.textContent = `${gridSlider.value} X ${gridSlider.value}`;
